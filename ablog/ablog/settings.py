053f41e331f0b4dab4f66fc09de16f0e6c2aa96b
omiiii21 <omengshetti@gmail.com>
2022-02-23 14:27:52 +0530

    commit 1

--- ablog/ablog/settings.py
+++ ablog/ablog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kqd36m@y6=x7$v+9!tudr2eg9g6q@a9h&yqgcvea4w5c19=n-+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 STATICFILES_DIRS=[
     "C:/Users/Dell/Desktop/PBL/ablog/theblog/static",
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
