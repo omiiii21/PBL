491f407d722b8d105e5914e36ce205d7b6352ce1
omiiii21 <omengshetti@gmail.com>
2022-02-23 14:28:42 +0530

    new

--- ablog/ablog/urls.py
+++ ablog/ablog/urls.py
@@ -5,9 +5,14 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('theblog.urls')),
     path('members/',include('django.contrib.auth.urls')),
     path('members/',include('members.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
