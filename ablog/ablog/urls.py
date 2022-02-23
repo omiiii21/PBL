835da6a711475e7a0bc9dd69794db1667695e709
omiiii21 <omengshetti@gmail.com>
2022-02-23 14:46:23 +0530

    something

--- ablog/ablog/urls.py
+++ ablog/ablog/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('',include('theblog.urls')),
     path('members/',include('django.contrib.auth.urls')),
     path('members/',include('members.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
